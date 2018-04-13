fc0bedf6e51872f099fa27a2923dbf81e9124a6c
mohita <mohitaunni@gmail.com>
2018-04-13 16:34:57 +0530

    catchupactivity added

---
+++ app/src/main/java/com/aikya/konnek2/ui/activities/catchup/CatchUpActivity.java
@@ -0,0 +1,38 @@
+package com.aikya.konnek2.ui.activities.catchup;
+
+import android.os.Bundle;
+
+import com.aikya.konnek2.R;
+import com.aikya.konnek2.ui.activities.base.BaseLoggableActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import butterknife.Bind;
+
+public class CatchUpActivity extends BaseLoggableActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    @Bind(R.id.map)
+    SupportMapFragment mapFragment;
+    @Override
+    protected int getContentResId() {
+        return R.layout.activity_catchup;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
